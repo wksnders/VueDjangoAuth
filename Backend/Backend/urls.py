6f7ae2fcc15a6f6d7bbc8e90b64d3ff011b9636b
wksnders <42388072+wksnders@users.noreply.github.com>
2024-11-14 21:11:01 -0700

    url backend update

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('goats.urls')),
-    path('session/', include('session.urls')),
+    path('api/session/', include('session.urls')),
 ]
