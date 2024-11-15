f6867a2045fc94345b688d07f19d813f34abf048
wksnders <42388072+wksnders@users.noreply.github.com>
2024-11-14 23:08:54 -0700

    add baseuser

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'session',
 ]
 
+AUTH_USER_MODEL = "User.CustomUser"
 
 
 MIDDLEWARE = [
@@ -129,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
